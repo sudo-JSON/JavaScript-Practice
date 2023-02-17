266ff0211ea59550820cdadd0d3dd1254f1a1188
AKSHAY <forakshay.dev@gmail.com>
2023-02-17 19:46:28 +0530

    Create 1.js

---
+++ introduction to js/1.js
@@ -0,0 +1,13 @@
+//SINGLE LINE COMMENT
+
+//filling in the blanks
+
+ console.log("hello world");
+
+ console.log("%chello world", "color:red; font-size: 100px");
+ 
+
+var a = "hello world";
+
+
+console.log(a)
