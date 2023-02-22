0b929bcb5dbf3e51a9dfff3f430f07b52accd69f
Shivam <shivam740945@gmail.com>
2023-02-22 21:45:41 +0530

    Some correction + gitignore file

---
+++ projects/1.js
@@ -0,0 +1,30 @@
+// operator , 2 numbers , result 
+// input , result output
+// if else 
+// result display console.log
+
+const operator = prompt('Enter Operator (either +,-,*,/):');
+
+const num1 = parseFloat(prompt('enter 1st number:'));
+const num2 = parseFloat(prompt('enter 2nd number:'));
+
+let result;
+// conditions
+//if else 
+// multiple available choices me se koi 1 choose karna ho 
+
+if (operator == '+') {
+    result = num1+num2;
+    }
+else if (operator == '-') {
+    result = num1-num2;
+    }
+else if (operator == '*') {
+    result = num1*num2;
+    }
+else if(operator == '/')  {
+    result = num1/num2;
+    }
+console.log(num1, operator, num2, '=', result);
+
+// Still facing problem in executing JS Files, Fix this Issue
