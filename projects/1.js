6596ef3764ff093bd821065f68ca7e47e69247dd
Shivam <shivam740945@gmail.com>
2023-02-24 11:28:36 +0530

    JS-prompt Issuse solved
    Added details in readme file, deleted

--- projects/1.js
+++ projects/1.js
@@ -3,10 +3,12 @@
 // if else 
 // result display console.log
 
+const prompt = require("prompt-sync")(); //This is required for prompt to work
+
 const operator = prompt('Enter Operator (either +,-,*,/):');
 
 const num1 = parseFloat(prompt('enter 1st number:'));
-const num2 = parseFloat(prompt('enter 2nd number:'));
+const num2 = parseFloat(prompt('enter 2nd number:')); 
 
 let result;
 // conditions
@@ -28,3 +30,4 @@ else if(operator == '/')  {
 console.log(num1, operator, num2, '=', result);
 
 // Still facing problem in executing JS Files, Fix this Issue
+// ** Issue solved, using const prompt = require("prompt-sync")(); at the top
